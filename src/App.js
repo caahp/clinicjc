3c5d64c8f2483994881c099615765bbf17496062
Caroline Pereira <carolline.ps@hotmail.com>
2023-09-04 22:50:41 -0400

    att components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Logo from './components/layout/Logo';
+import Logo from './components/layout/Logo/Logo';
 import Header from './components/layout/Header/Header';
 
 function App() {
