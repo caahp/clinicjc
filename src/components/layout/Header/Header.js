e3f162e5810d2f6773acc81c7e36598ba9b5b087
Caroline Pereira <carolline.ps@hotmail.com>
2023-09-13 15:30:32 -0400

    responsive header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Opcao = styled.li`
     color: #ffffff;
@@ -11,25 +11,42 @@ const Opcao = styled.li`
     text-align: center;
     height: 100%;
     padding: 0 10px;
+    padding-left: 40px;
     cursor: pointer;
     min-width: 120px;
-`
+
+    @media (max-width: 768px) {
+        font-size: 10px; /* Reduza o tamanho da fonte em telas menores */
+        min-width: auto; /* Remova a largura mínima em telas menores */
+        flex-grow: 1; /* Faça com que as opções cresçam para ocupar o espaço disponível */
+        flex-basis: 0; /* Redefina a base flex para permitir o crescimento igual */
+        margin-top: -15px; /* Ajuste a margem superior negativa para mover o texto para cima em telas menores */
+        margin-right: -12px; /* Ajuste o espaçamento entre as opções em telas menores */
+    }
+`;
 
 const Opcoes = styled.ul`
     display: flex;
+    margin-top: 35px;
     margin-bottom: 0px;
     margin-left: 600px;
-`
+    height: 65px;
 
-const textoOpcoes = ['HOME', 'PROCEDIMENTOS ⇩', 'CONTATO']
+    @media (max-width: 768px) {
+        margin-left: 100px; /* Remova a margem esquerda em telas menores */
+    }
+`;
+
+const textoOpcoes = ['HOME', 'PROCEDIMENTOS', 'CONTATO']; /* Ajuste o texto para incluir a palavra "PROCEDIMENTO" */
 
 function Header() {
-    return(
-    <Opcoes>
-            { textoOpcoes.map((texto) => (
-              <Opcao><p>{texto}</p></Opcao>
-            ) )}
-        </Opcoes> )
+    return (
+        <Opcoes>
+            {textoOpcoes.map((texto, index) => (
+                <Opcao key={index}><p>{texto}</p></Opcao>
+            ))}
+        </Opcoes>
+    );
 }
 
 export default Header;
--- src/components/layout/Logo/Logo.js
+++ src/components/layout/Logo/Logo.js
@@ -4,23 +4,37 @@ import styled from 'styled-components';
 const LogoContainer = styled.div`
     display: flex;
     width: 120px;
-    height: 120px; 
+    height: 120px;
     color: #ffffff;
+
+    @media (max-width: 768px) {
+        width: 80px; /* Reduza a largura em telas menores */
+        height: 80px; /* Reduza a altura em telas menores */
+        position: absolute; /* Posição absoluta em telas menores */
+        top: 10px; /* Distância do topo */
+        left: 20px; /* Distância da esquerda */
+    }
 `
+
 const LogoImage = styled.img`
     margin-right: 10px;
     margin-left: 20px;
     margin-top: 0px;
+    width: 100%;
+    height: auto;
+
+    @media (max-width: 768px) {
+        margin-left: 10px; /* Ajuste a margem em telas menores */
+    }
 `
 
-
 function Logo() {
-    return ( 
-    <LogoContainer>
-        <LogoImage 
-        src={logo} 
-        alt = 'logo'/>
-    </LogoContainer>
+    return (
+        <LogoContainer>
+            <LogoImage
+                src={logo}
+                alt='logo' />
+        </LogoContainer>
     );
 }
 
